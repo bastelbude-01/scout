91185410d809efc7296c120c1865d4313ef92e01
bastelbude-01 <messerschmidt.markus@web.de>
2023-01-05 22:36:25 +0100

    save this

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## Robot Package Scout
 
-Outdoor Rover
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -10,20 +10,25 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='scout' #<--- CHANGE ME
+    package_name='scout' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items() #  , 'use_ros2_control': 'true'
+                )]), launch_arguments={'use_sim_time': 'true'}.items() #, 'use_ros2_control': 'true'
     )
+
+    #camera = IncludeLaunchDescription(
+    #            PythonLaunchDescriptionSource([os.path.join(
+    #                get_package_share_directory(package_name),'launch','camera.launch.py'
+    #            )]) 
+    #)
 
     #joystick = IncludeLaunchDescription(
     #            PythonLaunchDescriptionSource([os.path.join(
@@ -31,13 +36,13 @@ def generate_launch_description():
     #            )]), launch_arguments={'use_sim_time': 'true'}.items()
     #)
 
-    #gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])#,
-                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -81,6 +86,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        #camera,
         #joystick,
         gazebo,
         spawn_entity,
